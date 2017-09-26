795561f103f16196a80f0dd683240d43bf20ac82
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-26 12:33:13 -0400

    Create intance method balance, reader and writer instance methods

--- exercise.rb
+++ exercise.rb
@@ -1,6 +1,18 @@
 class BankAccount
 @@interest_rate = 0.125
-@@accounts =[]  
+@@accounts =[]
+
+def initialize
+  @balance = 0
+end
+
+def balance
+  @balance
+end
+
+def deposit=(deposit)
+  @balance += deposit
+end
 
 
 
