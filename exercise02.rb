7f70b0db134ee7916e19d2361febe8a4e828aac2
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-26 17:31:16 -0400

    Initialized, created browse and available methods

--- exercise02.rb
+++ exercise02.rb
@@ -6,18 +6,20 @@ class Book
     @due_date = Time.now + time
   end
 
-  def initialize
-
+  def initialize(title, author, isbn_13)
+    @title  = title
+    @author = author
+    @isbn_13 = isbn_13
   end
 
   def self.create(title, author, isbn_13)
-    new_book = Book.new
+    new_book = Book.new(title, author, isbn_13)
     @@on_shelf << new_book
     new_book
   end
 
   def self.available
-    p @@on_shelf
+    @@on_shelf
   end
 
   def self.borrowed
@@ -25,6 +27,8 @@ class Book
   end
 
   def self.browse
+    @@on_shelf.sample
+
     # This class method should return a random book from @@on_shelf
     # (you may need to consult the Array docs to figure out how to do this).
   end
@@ -58,3 +62,20 @@ end
 sister_outsider = Book.create("Sister Outsider", "Audre Lorde", "9781515905431")
 aint_i = Book.create("Ain't I a Woman?", "Bell Hooks", "9780896081307")
 if_they_come = Book.create("If They Come in the Morning", "Angela Y. Davis", "0893880221")
+puts Book.browse.inspect # #<Book:0x00555e82acdab0 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221"> (this value may be different for you)
+puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
+puts Book.browse.inspect # #<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307"> (this value may be different for you)
+puts Book.available.inspect # [#<Book:0x00555e82acde20 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431">, #<Book:0x00555e82acdce0 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00555e82acdab0 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
+# puts Book.borrowed.inspect # []
+# puts sister_outsider.lent_out? # false
+# puts sister_outsider.borrow # true
+# puts sister_outsider.lent_out? # true
+# puts sister_outsider.borrow # false
+# puts sister_outsider.due_date # 2017-02-25 20:52:20 -0500 (this value will be different for you)
+# puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">]
+# puts Book.borrowed.inspect # [#<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=2017-02-25 20:55:17 -0500>]
+# puts Book.overdue.inspect # []
+# puts sister_outsider.return_to_library # true
+# puts sister_outsider.lent_out? # false
+# puts Book.available.inspect # [#<Book:0x00562314676118 @title="Ain't I a Woman?", @author="Bell Hooks", @isbn="9780896081307">, #<Book:0x00562314675fd8 @title="If They Come in the Morning", @author="Angela Y. Davis", @isbn="0893880221">, #<Book:0x00562314676208 @title="Sister Outsider", @author="Audre Lorde", @isbn="9781515905431", @due_date=nil>]
+# puts Book.borrowed.inspect # []
