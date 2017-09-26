ce572238e5c4c95565b60c1b9cb529958b9ab5c6
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-26 12:39:53 -0400

    Define create account class method

--- exercise.rb
+++ exercise.rb
@@ -2,22 +2,26 @@ class BankAccount
 @@interest_rate = 0.125
 @@accounts =[]
 
-def initialize
-  @balance = 0
-end
+  def initialize
+    @balance = 0
+  end
 
-def balance
-  @balance
-end
+  def balance
+    @balance
+  end
 
-def deposit=(deposit)
-  @balance += deposit
-end
+  def deposit=(deposit)
+    @balance += deposit
+  end
 
+  def withdraw=(withdraw)
+    @balance -= withdraw
+  end
 
-
-
-
+  def self.create(new_account)
+    new_account = BankAccount.new
+    @@accounts << new_account
+  end
 
 
 
