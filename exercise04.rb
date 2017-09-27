9eda73b0cbb459ffa230f4bfdef24ec5e630ff5e
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-27 16:31:57 -0400

    Define sunrise method

--- exercise04.rb
+++ exercise04.rb
@@ -4,7 +4,7 @@ class Vampire
   def initialize(name, age)
     @name = name
     @age = age
-    @in_coffin = true
+    @in_coffin = false
     @drank_blood_today = false
   end
 
@@ -32,6 +32,10 @@ class Vampire
     @age = age
   end
 
+  def self.coven
+    @@coven
+  end
+
   def in_coffin=(in_coffin)
     @in_coffin = in_coffin
   end
@@ -40,7 +44,7 @@ class Vampire
     @drank_blood_today = drank_blood_today
   end
 
-  def create
+  def self.create(name, age)
     new_vampire = Vampire.new(name, age)
     @@coven << new_vampire
     new_vampire
@@ -50,4 +54,37 @@ class Vampire
     @drank_blood_today = true
   end
 
+  def self.sunrise
+    new_coven = []
+    @@coven.each do |vampire|
+      if vampire.in_coffin == true || vampire.drank_blood_today == true
+        new_coven << vampire
+      end
+    end
+    @@coven = new_coven
+  end
+
+  def self.sunset
+    @drank_blood_today = false
+    @in_coffin = false
+  end
+
+  def go_home
+    @in_coffin = true
+  end
+
 end
+
+
+Vampire.create("Morty", 107)
+Vampire.create("Juan", 99)
+Vampire.create("Mary", 33)
+Vampire.create("Lola", 139)
+p Vampire.coven
+p "-------------------------"
+Vampire.coven[0].drink_blood
+Vampire.coven[1].go_home
+p Vampire.coven
+p "-------------------------"
+Vampire.sunrise
+p Vampire.coven
