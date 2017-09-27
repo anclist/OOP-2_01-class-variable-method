9367f69f61255f0dad63513d56ac4fb7fedaca1e
ANTONIO <ANCLIST@GMAIL.COM>
2017-09-26 20:49:58 -0400

    Add outputs

--- exercise03.rb
+++ exercise03.rb
@@ -33,7 +33,7 @@ class Zombie
   end
 
   def outrun_zombie?
-
+    
   end
 
   def survive_attack?
@@ -45,6 +45,7 @@ class Zombie
   end
 
   def self.new_day
+    Zombie.spawn
     Zombie.some_die_off
     Zombie.increase_plague_level
     Zombie.spawn
@@ -70,10 +71,25 @@ class Zombie
 end
 
 
-Zombie.spawn
-puts Zombie.all#.inspect
-
-puts "------------------"
-
 Zombie.new_day
-puts Zombie.all#.inspect
+puts Zombie.all.inspect
+fighting_zombie = Zombie.all[rand(Zombie.all.length)]
+# puts Zombie.all[rand(Zombie.all.length)].inspect
+
+# puts Zombie.all.inspect # []
+# Zombie.new_day
+# puts Zombie.all.inspect # [#<Zombie:0x005626ecc5ebd0 @speed=4, @strength=0>, #<Zombie:0x005626ecc5eba8 @speed=0, @strength=4>, #<Zombie:0x005626ecc5eb80 @speed=4, @strength=4>]
+# zombie1 = Zombie.all[0]
+# zombie2 = Zombie.all[1]
+# zombie3 = Zombie.all[2]
+# puts zombie1.encounter # You are now a zombie
+# puts zombie2.encounter # You died
+# puts zombie3.encounter # You died
+# Zombie.new_day
+# puts Zombie.all.inspect # [#<Zombie:0x005626ecc5e1f8 @speed=0, @strength=0>, #<Zombie:0x005626ecc5e180 @speed=3, @strength=3>, #<Zombie:0x005626ecc5e158 @speed=1, @strength=2>, #<Zombie:0x005626ecc5e090 @speed=0, @strength=4>]
+# zombie1 = Zombie.all[0]
+# zombie2 = Zombie.all[1]
+# zombie3 = Zombie.all[2]
+# puts zombie1.encounter # You got away
+# puts zombie2.encounter # You are now a zombie
+# puts zombie3.encounter # You died
